fed8672f9f3422da8d7415d1a45b93e00254414e
mtiday <github@tidayventures.com>
2020-12-15 19:53:57 -0700

    Created the base of the main program. Added linux_name_checker to the menu.

--- file_manipulator.py
+++ file_manipulator.py
@@ -2,3 +2,37 @@
 I expect this program to grow as I, and the community, grows.
 """
 
+import time
+import linux_name_checker
+
+
+def start():
+    """Get user input and execute actions on files and folders"""
+    while True:
+        print('\nPlease choose from the following list:')
+        print('"1" to find and replace text in text files')
+        print('"2" to scan Linux folders for files that would \
+        cause duplicates if copied to Windows.')
+        program_to_open = str(input('"Q" to quit and end the program. '))
+
+        if program_to_open in ('Q', 'q'):
+            exit_program()
+        elif program_to_open == '1':
+            pass
+        elif program_to_open == '2':
+            print(linux_name_checker.start())
+        else:
+            print(f'"{program_to_open}" wasn\'t a valid entry. '
+                  f'please try again.\n')
+
+
+def exit_program():
+    """Exit the program cleanly."""
+    print('\nHave a great day!!')
+    time.sleep(5)
+    raise SystemExit
+
+
+if __name__ == "__main__":
+    start()
+
